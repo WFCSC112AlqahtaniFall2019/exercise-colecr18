c030609858b08cbd9e10a3f33d8e15bee67d6aa5
clairecoleman <colecr18@wfu.edu>
2019-09-05 15:14:09 -0400

    In class exercise 9/5

--- main.cpp
+++ main.cpp
@@ -1,25 +1,30 @@
 #include <iostream>
+#include <vector>
 using namespace std;
 
 int main() {
     const int NUM_ELEMENTS = 8;   // Number of elements
-    int revVctr[8];               // User values
-    int i;                        // Loop index
+    vector<int> revVctr(NUM_ELEMENTS);               // User values
+    unsigned int i;                        // Loop index
+    int temp;
 
     cout << "Enter " << NUM_ELEMENTS << " integer values..." << endl;
     for (i = 0; i < NUM_ELEMENTS; ++i) {
-        cin >> revVctr[i];
+        cin >> revVctr.at(i);
     }
 
     // Reverse
-    for (i = 0; i < NUM_ELEMENTS; ++i) {
-        revVctr[i] = revVctr[NUM_ELEMENTS - i]; // Swap
+    for (i = 0; i < NUM_ELEMENTS / 2; ++i) {
+        temp = revVctr.at(i);
+        revVctr.at(i) = revVctr.at(NUM_ELEMENTS - i - 1); // Swap
+        revVctr.at(NUM_ELEMENTS - i - 1) = temp;
+
     }
 
     // Print values
     cout << endl << "New values: ";
     for (i = 0; i < NUM_ELEMENTS; ++i) {
-        cout << " " << revVctr[i];
+        cout << " " << revVctr.at(i);
     }
     cout << endl;
 
